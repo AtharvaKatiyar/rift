2f54d3cb2b1d311a5b861ecb9e1d2b2b901e6dce
AtharvaKatiyar <atharvakatiyar12345@gmail.com>
2026-07-28 20:11:53 +0530

    migrations created for creating password reset tokens

---
+++ backend/internal/auth/password_reset.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"time"
+)
+
+const (
+	PasswordResetTokenBytes = 32
+
+	PasswordResetTokenTTL =
+		30 * time.Minute
+)
+
+func GeneratePasswordResetToken() (
+	rawToken string,
+	tokenHash string,
+	err error,
+) {
+
+	randomBytes := make(
+		[]byte,
+		PasswordResetTokenBytes,
+	)
+
+	_, err = rand.Read(
+		randomBytes,
+	)
+
+	if err != nil {
+		return "", "", err
+	}
+
+	rawToken =
+		base64.RawURLEncoding.EncodeToString(
+			randomBytes,
+		)
+
+	hash := sha256.Sum256(
+		[]byte(rawToken),
+	)
+
+	tokenHash =
+		hex.EncodeToString(
+			hash[:],
+		)
+
+	return
+}
+
+func HashPasswordResetToken(
+	token string,
+) string {
+
+	sum := sha256.Sum256(
+		[]byte(token),
+	)
+
+	return hex.EncodeToString(
+		sum[:],
+	)
+}
